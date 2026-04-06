37bff41aa565dc9088e3a518d691711ed5255f25
armar02-eng <armaros.02@gmail.com>
2026-04-06 15:05:45 -0600

    Finalize tips analysis structure with project-local charts

--- analysis/tips_analysis/tips_analysis.py
+++ analysis/tips_analysis/tips_analysis.py
@@ -1,19 +1,33 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load data
-df = pd.read_csv('../data/tips.csv')
+pd.read_csv('../../data/tips.csv')
 
-# Total revenue per day
+# Revenue per day
 revenue_per_day = df.groupby('day')['total_bill'].sum()
-print("Revenue Per Day:")
-print(revenue_per_day)
 
-# Average tips by gender
+# Plot revenue per day
+revenue_per_day.plot(kind='bar', title='Revenue per Day')
+plt.xlabel('Day')
+plt.ylabel('Total Revenue')
+plt.savefig('revenue_per_day.png')
+plt.close()
+
+# Average tip by gender
 tip_by_gender = df.groupby('sex')['tip'].mean()
-print("\nAverage Tips By Gender:")
-print(tip_by_gender)
 
-# Average tips by group size
+tip_by_gender.plot(kind='bar', title='Average Tip by Gender')
+plt.xlabel('Gender')
+plt.ylabel('Average Tip')
+plt.savefig('tips_by_gender.png')
+plt.close()
+
+# Average tip by group size
 tip_by_size = df.groupby('size')['tip'].mean()
-print("Average Tips By Group Size:")
-print(tip_by_size)
+
+tip_by_size.plot(kind='line', marker='o', title='Average Tip by Group Size')
+plt.xlabel('Group Size')
+plt.ylabel('Average Tip')
+plt.savefig('tips_by_size.png')
+plt.close()
